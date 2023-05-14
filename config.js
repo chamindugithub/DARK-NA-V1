e24b9cb108d6c61e727272d0a5210ca2889e4f18
chamindu git <110526143+chamindugithub@users.noreply.github.com>
2023-05-14 16:11:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀ
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? 'MVRkRUt_astro_Rd1Y=' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=Z24khZJS#UkwwYiB7LUz_ogiAwU1_VRbc2yOdiYL8KmInFCsKFFk' : process.env.SESSION_ID
 };
